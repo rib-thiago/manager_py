4e6ebf4de00ec63f333acf22f400b89f6cdf75eb
Thiago Ribeiro <mackandalls@gmail.com>
2023-09-15 09:28:37 -0300

    Testando o suporte à instalação

--- pyenv_create.sh
+++ pyenv_create.sh
@@ -29,15 +29,13 @@
 #
 
 # 
-# TODO: Testar se script recebeu versão da linha de comando, se não recebeu, exibir mensagem de ajuda 
-# TODO: Testar se versão informada já está instalada, se sim, mostrar versões instaladas
-# TODO: Instalar versão do python instalada
+# TODO: Instalar versão do python solicitada
 # TODO: Usar versão instalada do para gerar a variável usada para criar o nome do diretório
 # TODO: Criar diretório com o nome da versão;
 # TODO: Exibir mensagem de confirmação
 # TODO: Testar se o pyenv está instalado e, do contrário, exibir mensagem de erro
 #
-# FIXME: None.
+# FIXME: Implementar uma barra de progresso quando da instalação do python
 #
 ################################################################################
 
@@ -49,12 +47,14 @@ BLINK="\033[5m"
 RESET="\033[0m"
 
 
+## Validações:
+
 # Verificar se a versão da linha de comando foi fornecida
 # Verificação da quantidade de argumentos passados na linha de comando ($#). 
 # Se nenhum argumento for fornecido, exibe uma mensagem de uso e sai com um código de erro.
 if [ $# -eq 0 ]; then
-  echo -e "${RED}Uso: $0 <versão_python>${RESET}"
-  exit 1
+    echo -e "${RED}Uso: $0 <versão_python>${RESET}"
+    exit 1
 fi
 
 # Verificar se a versão informada já está instalada
@@ -62,19 +62,38 @@ fi
 # Se já instalada, exibe uma mensagem informando que a versão já está instalada e 
 # mostra as versões Python instaladas usando pyenv versions.
 if pyenv versions --bare | grep -q "$1"; then
-  echo -e "${YELLOW}A versão Python $1 já está instalada. Versões instaladas:"
-  pyenv versions
-  echo -e "${RESET}"
-  exit 1
+    echo -e "${YELLOW}A versão Python $1 já está instalada. Versões instaladas:"
+    pyenv versions
+    echo -e "${RESET}"
+    exit 1
 fi
 
-# Instalar a versão Python desejada
-pyenv install $1
+## Instalação
+
+# Pedir permissão para instalar em segundo plano
+read -p "Deseja instalar a versão Python $1 em segundo plano? (y/n): " answer
+echo -e "${YELLOW}"
+if [ "$answer" == "y" ]; then
+    # Instalar a versão Python desejada
+    if pyenv install $1 &>/dev/null; then
+        echo -e "${GREEN}A versão Python $1 foi instalada com sucesso em $HOME/.pyenv/$1.  ${RESET}"
+    else
+        echo -e "${RED}Erro ao instalar a versão Python $1:  ${RESET}"
+    fi
+else
+    if pyenv install $1; then
+        echo -e "${GREEN}A versão Python $1 foi instalada com sucesso em $HOME/.pyenv/$1.  ${RESET}"
+    else
+        echo -e "${RED}Erro ao instalar a versão Python $1:  ${RESET}"
+    fi
+fi
 
 # Converter a versão para um nome de diretório
 version_name="$(echo $1 | tr '.' '_')_py"
+read -p "Calma, respira!!!"
+exit 1
 
 # Criar o diretório
-mkdir -p ~/python_versions/$version_name
-
-echo "A versão Python $1 foi instalada com sucesso em ~/python_versions/$version_name."
+# mkdir -p ~/${version_name}
+    
+    
