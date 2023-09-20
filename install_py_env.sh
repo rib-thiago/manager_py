d44f2b9ed62d076838845405cc1f411de81908b4
Thiago Ribeiro <mackandalls@gmail.com>
2023-09-20 16:38:35 -0300

    Versão 0.0.2: Checando por python e pip antes de seguir

--- install_py_env.sh
+++ install_py_env.sh
@@ -13,7 +13,7 @@
 #
 # Author: Thiago Ribeiro
 # Date: 2023-09-15
-# Version: 0.0.1
+# Version: 0.0.2
 
 #
 # Usage:
@@ -32,101 +32,100 @@
 # Version   Date          Author          Description
 # -------   ----          ------          -----------
 # 0.0.1     [2023-09-15]  Thiago Ribeiro  Initial release.
+# 0.0.2     [2023-09-20]  Thiago Ribeiro  Simplication of the logic,
+#                                         and checking python then pip before 
+#                                         trying to install pipx, poetry and pyenv 
 
 
 # 
 # TODO: Verificar as Dependências para instalar o pynv, pipx e poetry
 
 #
-# FIXME: O pip não vem instalado no Python do Ubuntu, então é preciso instalar
-#        ele antes. 
+# FIXME:  
 
 
 ################################################################################
 
-## Paleta de Cores
-RED="\033[31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
-BLINK="\033[5m"
-RESET="\033[0m"
 
-## Validação
-
-# Testar se o pip está instalado
-if ! command -v "pip" >/dev/null 2>&1; then
-    echo "O pip não está instalado. Instalando o pip..."
-    sudo apt install python3-pip
-fi
-
-# Testar se cowsay está instalado
-if ! command -v "cowsay" >/dev/null 2>&1; then
-    pip install cowsay &>/dev/null
-fi
 
 # Lista de dependências e seus binários associados
 declare -A dependencies
-dependencies["git"]="git"
+dependencies["python3"]="python3"
+dependencies["pip"]="pip"
+dependencies["cowsay"]="cowsay"
 dependencies["pipx"]="pipx"
 dependencies["pyenv"]="pyenv"
 dependencies["poetry"]="poetry"
 
-# Verificar e instalar as dependências ausentes
-missing_dependencies=()
+# Definir a ordem desejada para a instalação das dependências
+install_order=("pip" "cowsay" "pipx" "poetry" "pyenv")
 
+# Verificar e remover dependências já instaladas
 for dep in "${!dependencies[@]}"; do
-    binary="${dependencies[$dep]}"
-    if ! command -v "$binary" >/dev/null 2>&1; then
-        missing_dependencies+=("$dep")
+    if command -v "${dependencies[$dep]}" >/dev/null 2>&1; then
+        unset dependencies["$dep"]
     fi
 done
 
-if [ ${#missing_dependencies[@]} -gt 0 ]; then
-    echo -e "${YELLOW}As seguintes dependências estão ausentes: ${missing_dependencies[*]}${RESET}"
+if [ ${#dependencies[@]} -gt 0 ]; then
+    echo -e "As seguintes dependências estão ausentes: ${dependencies[@]}"
     read -p "Deseja instalá-las agora? (Y/N): " install_choice
     if [[ "$install_choice" =~ [Yy] ]]; then
         # Adicione os comandos de instalação aqui para as dependências ausentes
-        for dep in "${missing_dependencies[@]}"; do
-            case "$dep" in
-                "pipx")
-                    # Comandos para instalar pipx
-                    pip install --user pipx &>/dev/null
-                    python3 -m pipx ensurepath &>/dev/null
-                    teste_install=$(cowsay -t "Instalação do pipx bem sucedida")
-                    echo -e "${GREEN} ${teste_install} ${RESET}"
-                    ;;
-                "pyenv")
-                    # Comandos para instalar pyenv
-                    git clone https://github.com/pyenv/pyenv.git ~/.pyenv &>/dev/null
-                    # variáveis de ambiente do .bashrc
-                    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-                    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-                    echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
-                    if pyenv --version &>/dev/null; then
-                        teste_install=$(cowsay -t "Instalação do pyenv bem sucedida")
-                        echo -e "${GREEN} ${teste_install} ${RESET}"
-                    fi
-                    ;;
-                "poetry")
-                    # Comandos para instalar poetry
-                    curl -sSL https://install.python-poetry.org | python3 - --git https://github.com/python-poetry/poetry.git@master &>/dev/null
-                    if poetry --version &>/dev/null; then
-                        teste_install=$(cowsay -t "Instalação do poetry bem sucedida")
-                        echo -e "${GREEN} ${teste_install} ${RESET}"
-                    fi
-                    ;;
-                *)
-                    echo -e "${RED}Comandos de instalação para $dep não especificados. ${RESET}"
-                    ;;
-            esac
+        for dep in "${install_order[@]}"; do
+            if [[ -n "${dependencies[$dep]}" ]]; then
+                case "$dep" in
+                    "python3")
+                        sudo apt install -y python3
+                        [[ $? -ne 0 ]] && echo "Falhou" && exit 1
+                        ;;
+                    "pip")
+                        sudo apt install python3-pip
+                        [[ $? -ne 0 ]] && echo "Falhou" && exit 1
+                        ;;
+                    "cowsay")
+                        pip install cowsay
+                        [[ $? -ne 0 ]] && echo "Falhou" && exit 1
+                        ;;
+                    "pipx")
+                        # Comandos para instalar pipx
+                        pip install --user pipx &>/dev/null
+                        python3 -m pipx ensurepath &>/dev/null
+                        teste_install=$(cowsay -t "Instalação do pipx bem sucedida")
+                        echo -e "${teste_install}"
+                        ;;
+                    "pyenv")
+                        # Comandos para instalar pyenv
+                        git clone https://github.com/pyenv/pyenv.git ~/.pyenv &>/dev/null
+                        # variáveis de ambiente do .bashrc
+                        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+                        echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+                        echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+                        source .bashrc
+                        if pyenv --version &>/dev/null; then
+                            teste_install=$(cowsay -t "Instalação do pyenv bem sucedida")
+                            echo -e "${teste_install}"
+                        fi
+                        ;;
+                    "poetry")
+                        # Comandos para instalar poetry
+                        curl -sSL https://install.python-poetry.org | python3 - --git https://github.com/python-poetry/poetry.git@master &>/dev/null
+                        if poetry --version &>/dev/null; then
+                            teste_install=$(cowsay -t "Instalação do poetry bem sucedida")
+                            echo -e "${teste_install}"
+                        fi
+                        ;;
+                    *)
+                        echo -e "Comandos de instalação para $dep não especificados."
+                        ;;
+                esac
+            fi
         done
     else
-        echo -e "${RED}Você optou por não instalar as dependências ausentes. ${RESET}"
+        echo -e "Você optou por não instalar as dependências ausentes."
         exit 2
     fi
 else
-    echo -e "${GREEN}Todas as dependências estão instaladas.${RESET}"
+    echo -e "Todas as dependências estão instaladas."
     exit 0
 fi
-
-
